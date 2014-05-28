6baf3601bbf08d56e4e13c8109db2ca4635cf3f5
Guillermo Tenaschuk <guillermo@dinkuminteractive.com>
2014-05-28 20:38:49 -0300

    Support thumbnails in events

--- app/scripts/admin/controllers/event.js
+++ app/scripts/admin/controllers/event.js
@@ -94,3 +94,14 @@ angular.module('mavenEventsApp').controller('PricesCtrl', ['$scope', '$http', fu
 	}]);
 
  
+angular.module('mavenEventsApp').controller('VariationCtrl', ['$scope', '$http', function($scope, $http) {
+//		var chatbox = angular.element(document.getElementById('publish'));
+//		chatbox.val('holaaa');
+//		
+//		var postForm = angular.element(document.getElementById('post'));
+//		angular.element.bind("submit", function(event) {
+//			console.log('binded');
+//		}
+
+		//ng-disabled="formSettings.$invalid"
+}]);
