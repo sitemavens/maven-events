dfa582d0b7ca645787f4f4a637def457483d9b8a
kamalcha <mail@kamalchaneman.com>
2014-06-10 18:03:07 +0700

    fix alignment in event edit page
    Signed-off-by: kamalcha <mail@kamalchaneman.com>

--- app/admin/views/wp/event-view.php
+++ app/admin/views/wp/event-view.php
@@ -6,73 +6,104 @@
 
 	<tabset>
 		<tab heading="General">
-			<div class="form-horizontal"  >
+			<div class="form-horizontal" style="margin:15px 0;">
 				<div class="form-group"  >
-					<label for=""  >Price:</label>		
-					<input  type="input" ng-model="event.price" name="mvn[event][price]"  />
+					<label for="" class="col-sm-2 control-label">Price:</label>
+					<div class="col-sm-2">
+						<input class="form-control" type="input" ng-model="event.price" name="mvn[event][price]"  />
+					</div>
 				</div>
 				<div class="form-group"  >
-					<label for=""  >Registration Start:</label>		
-					<input required type="date" ng-model="event.registrationStartDate" name="mvn[event][registrationStartDate]"  />
+					<label for="" class="col-sm-2 control-label">Registration Start:</label>
+					<div class="col-sm-2">
+						<input class="form-control" required type="date" ng-model="event.registrationStartDate" name="mvn[event][registrationStartDate]"  />
+					</div>
 				</div>
 				<div class="form-group"  >
-					<label for="input-time">Registration End:</label>		
-					<input required ng-model="event.registrationEndDate" type="date" name="mvn[event][registrationEndDate]" />
+					<label for="input-time" class="col-sm-2 control-label">Registration End:</label>		
+					<div class="col-sm-2">
+						<input class="form-control" required ng-model="event.registrationEndDate" type="date" name="mvn[event][registrationEndDate]" />
+					</div>
 				</div>
 				<div class="form-group"  >
-					<label for="input-time">Event Start:</label>
-					<input required ng-model="event.eventStartDate" type="date" name="mvn[event][eventStartDate]" />
+					<label for="input-time" class="col-sm-2 control-label">Event Start:</label>
+					<div class="col-sm-2">
+						<input class="form-control" required ng-model="event.eventStartDate" type="date" name="mvn[event][eventStartDate]" />
+					</div>
 				</div>
 
 				<div class="form-group"  >
-					<label for="input-time">Event End:</label>
-					<input required ng-model="event.eventEndDate" type="date" name="mvn[event][eventEndDate]" />
-				</div>
-				<div class="checkbox"  >
-					<label >Allow group registration:
-						<input type="checkbox" value="1"  ng-model="event.allowGroupRegistration"   name="mvn[event][allowGroupRegistration]" />
-					</label>
-				</div>
-				<div class="form-group"  >
-					<label for="input-time">Max group registrants:</label>
-					<input type="text"  ng-model="event.maxGroupRegistrants"   name="mvn[event][maxGroupRegistrants]" />
-				</div>
-				<div class="form-group"  >
-					<label for="input-time">Attendee Limit:</label>
-					<input type="text" ng-model="event.attendeeLimit"  name="mvn[event][attendeeLimit]" />
+					<label for="input-time" class="col-sm-2 control-label">Event End:</label>
+					<div class="col-sm-2">
+						<input class="form-control" required ng-model="event.eventEndDate" type="date" name="mvn[event][eventEndDate]" />
+					</div>
 				</div>
 				<div class="form-group">
-					<label for="input-time">Closed:</label>
-					<input disabled="disabled" type="checkbox" value="1" ng-model="event.closed"  name="mvn[event][closed]" />
+					<div class="col-sm-offset-2 col-sm-2">
+						<div class="checkbox"  >
+							<label >Allow group registration:
+							<input class="form-control" type="checkbox" value="1"  ng-model="event.allowGroupRegistration"   name="mvn[event][allowGroupRegistration]" />
+							</label>
+						</div>
+					</div>
+				</div>
+				<div class="form-group"  >
+					<label for="input-time" class="col-sm-2 control-label">Max group registrants:</label>
+					<div class="col-sm-2">
+						<input class="form-control" type="text"  ng-model="event.maxGroupRegistrants"   name="mvn[event][maxGroupRegistrants]" />
+					</div>
+				</div>
+				<div class="form-group"  >
+					<label for="input-time" class="col-sm-2 control-label">Attendee Limit:</label>
+					<div class="col-sm-2">
+						<input class="form-control" type="text" ng-model="event.attendeeLimit"  name="mvn[event][attendeeLimit]" />
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="input-time" class="col-sm-2 control-label">Closed:</label>
+					<div class="col-sm-2">
+						<input class="form-control" disabled="disabled" type="checkbox" value="1" ng-model="event.closed"  name="mvn[event][closed]" />
+					</div>
 				</div>
 			</div>
 		</tab>
 		<tab heading="Variations" ng-controller="VariationsCtrl">
-			<button type="button" ng-click="addVariation()"  class="btn btn-default">Add Variation</button>
+			<div style="margin:15px 0;">
+			<button type="button" ng-click="addVariation()"  class="btn btn-default" style="margin-bottom:15px;">Add Variation</button>
 
 			<form name="priceForm" >
-				<div class="row" ng-repeat="variation in variations">
+				<div class="row form-horizontal" ng-repeat="variation in variations">
 					<div class="col-md-4">
 						<div class="form-group"  >
-							<label for=""  >Name:</label>
-							<input  type="text" ng-value="variation.name"  />
-							<button type="button" ng-click="deleteVariation(variation)" class="btn btn-danger btn-xs">Delete</button>
+							<label for="" class="col-sm-2 control-label">Name:</label>
+							<div class="col-sm-10">
+								<div class="col-sm-8">
+									<input class="form-control" type="text" ng-value="variation.name"  />
+								</div>
+								<button type="button" ng-click="deleteVariation(variation)" class="btn btn-danger">Delete</button>
+							</div>
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="form-group" ng-repeat="option in variation.options" >
-							<label for=""  >Name:</label>
-							<input  type="text" ng-model="option.name"  />
-							<button type="button" ng-click="deleteOption(variation, option)" class="btn btn-danger btn-xs">Delete</button>
+							<label for="" class="col-sm-2 control-label">Name:</label>
+							<div class="col-sm-10">
+								<div class="col-sm-8">
+									<input class="form-control" type="text" ng-model="option.name"  />
+								</div>
+								<button type="button" ng-click="deleteOption(variation, option)" class="btn btn-danger">Delete</button>
+							</div>
 						</div>
 
 					</div>
 				</div>
 
-				<div class='row' ng-repeat="variation in variations">
+				<div class='row form-group' ng-repeat="variation in variations">
 					<div class="col-md-4">
-						{{variation.name}}
-						<select ng-model="selectedVariation.selectedOption" ng-options="option.name for option in variation.options"></select>
+						<label for="" class="col-sm-2 control-label">{{variation.name}}</label>
+						<div class="col-sm-10">
+							<select class="form-control" ng-model="selectedVariation.selectedOption" ng-options="option.name for option in variation.options"></select>
+						</div>
 					</div>
 				</div>
 
@@ -87,7 +118,7 @@
 						</div>
 					</div>
 				</div>
-
+			</div>
 			</form>
 
 		</tab>
