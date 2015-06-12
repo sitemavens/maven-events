c27aa4161bb586270833bdeed92a467b37401033
Enzo Hernan Nicolorich <enzonicolorich@gmail.com>
2015-06-12 17:16:14 -0300

    Fix issue with variations

--- app/core/events-config.php
+++ app/core/events-config.php
@@ -29,7 +29,7 @@ class EventsConfig {
 
 		\Maven\Core\HookManager::instance()->addInit( array( __CLASS__, 'registerTypes' ) );
         \Maven\Core\HookManager::instance()->addAction( 'maven/cart/itemPaid/mavenevents', array( __CLASS__, 'paidEvent' ), 10, 1 );
-        \Maven\Core\HookManager::instance()->addAction( 'maven/cart/checkStock', array( __CLASS__, 'checkEventStock' ), 10, 1 );
+        \Maven\Core\HookManager::instance()->addAction( 'maven/cart/checkStock/mavenevents', array( __CLASS__, 'checkEventStock' ), 10, 1 );
 	}
 
 	static function registerTypes () {
@@ -293,14 +293,16 @@ class EventsConfig {
         $variationGroupManager = new \MavenEvents\Core\VariationGroupManager();
         $hasStock = true;
         foreach($orderItems as $orderItem){
-            if ($hasStock) {
-                $event = $eventManager->get( $orderItem->getThingId());
-                $variationGroup = $variationGroupManager->get($orderItem->getThingVariationId());
-                if ($event->isSeatsEnabled() && $event->getAvailableSeats() == 0) {
-                    $hasStock = false;
-                } else if ($variationGroup->getQuantity() < $orderItem->getQuantity()) {
-                	$hasStock = false;
-                }
+        	if ($orderItem->getPluginKey() === 'mavenevents') {
+	            if ($hasStock) {
+	                $event = $eventManager->get( $orderItem->getThingId());
+	                $variationGroup = $variationGroupManager->get($orderItem->getThingVariationId());
+	                if ($event->isSeatsEnabled() && $event->getAvailableSeats() == 0) {
+	                    $hasStock = false;
+	                } else if ($variationGroup->getQuantity() < $orderItem->getQuantity()) {
+	                	$hasStock = false;
+	                }
+	            }
             }
         }
         return $hasStock;
